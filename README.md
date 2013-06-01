5e1c6683f76fe098a2f5cc59641eea368277e8e7
JoshuaGlazer <vbunny@alum.mit.edu>
2013-06-01 16:56:52 -0700

    Initial Commit
    Things should work

--- README.md
+++ README.md
@@ -2,3 +2,45 @@ InspectorCalculator
 ===================
 
 Allows calculation inside Unity3D inspector fields
+
+Ever wished you could do simple math in an inspector field?  
+Ever wished that scale of 2.42 could easily be multiplied up by 1.5?  
+Ever wished that object were nudged over by 10 * sin( pi / 3 ) ?  
+Well now all of this is possible and more!
+
+Simply select any text field in an inspector and push ctrl-shift-e ( command-shift-e on mac ) or select Evaluate from the Edit menu.
+
+The value in the field will be copied into a new Calculator Window.  
+You can then type a simple mathematical expression into the window and press return.  
+The window will close and the result will be placed back in your original field, just like magic!  
+Well, Magic driven by lots and lots of FLOPS.<br>
+
+Currently supports these functions, with any others easily wrappable
+sin, cos, tan, asin, acos, atan, pow, exp, log, ln, +, *, -, /, % and more
+
+To install-
+just copy the contents of the Unity4/Assets folder into your project's Assets folder
+
+Released under the MIT License:
+
+The MIT License (MIT)
+
+Copyright (c) 2013 Joshua Glazer, Naked Sky Entertainment, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ Unity4/Assets/Inspector Calculator/Editor/CalculatorWindow.cs
@@ -0,0 +1,186 @@
+using System;
+using UnityEngine;
+using UnityEditor;
+using Microsoft.CSharp;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Text;
+using System.Reflection;
+
+public class CalculatorWindow : EditorWindow 
+{
+	
+	
+		
+	[MenuItem ("Edit/Calculate... %#e")]
+	static void Evaluate()
+	{
+		StaticInit();
+		
+		EditorWindow focusedWindow = EditorWindow.focusedWindow;
+		
+		if( focusedWindow != null )
+		{
+			//preserve this so we don't make anybody cry
+			string oldCopyBuffer = EditorGUIUtility.systemCopyBuffer;
+			
+			focusedWindow.SendEvent(EditorGUIUtility.CommandEvent("SelectAll"));
+			focusedWindow.SendEvent(EditorGUIUtility.CommandEvent("Copy"));
+			var oldKeyboardControlID = GUIUtility.keyboardControl;
+
+			CalculatorWindow calcWindow = CalculatorWindow.GetWindow( typeof( CalculatorWindow ) ) as CalculatorWindow;
+			calcWindow.Init( focusedWindow, oldKeyboardControlID, oldCopyBuffer );
+		}
+
+	}
+	
+	void Init( EditorWindow inOldFocusedWindow, int inOldKeyboardControlID, string inOldCopyBuffer )
+	{
+		
+		
+		_OldFocusedWindow = inOldFocusedWindow;
+		_ToEval = EditorGUIUtility.systemCopyBuffer;
+		_OldKeyboardControlID = inOldKeyboardControlID;
+		_OldCopyBuffer = inOldCopyBuffer;
+		
+	}
+	
+	
+	void OnGUI () 
+	{
+		
+		Event e = Event.current;
+		KeyCode keyCode = e.keyCode;
+		EventType eventType = e.type;
+		
+		GUI.SetNextControlName("EvalField");
+		
+        _ToEval = EditorGUILayout.TextField( "Evaluate: ", _ToEval);
+		
+		if( GUI.GetNameOfFocusedControl() == "" ) 
+		{
+		    GUI.FocusControl( "EvalField" );
+		}
+		
+
+		if( keyCode == KeyCode.Return && eventType == EventType.KeyDown )
+		{
+			//var evaluator : Evaluator = new Evaluator();
+			string val  = UserFriendlifyAndEvaluate( _ToEval );	
+			if( string.IsNullOrEmpty( val ) )
+			{
+				val = _ToEval;
+			}
+			EditorGUIUtility.systemCopyBuffer = val.ToString();
+			Close();
+			_OldFocusedWindow.Focus();
+			GUIUtility.keyboardControl = _OldKeyboardControlID;
+			_OldFocusedWindow.SendEvent(EditorGUIUtility.CommandEvent("SelectAll"));
+			_OldFocusedWindow.SendEvent(EditorGUIUtility.CommandEvent("Paste"));
+			//_OldFocusedWindow.SendEvent(Event.KeyboardEvent( "return" ) );
+			//_OldFocusedWindow.Repaint();
+			//GUIUtility.keyboardControl = _OldKeyboardControlID;
+			
+			//restore!
+			EditorGUIUtility.systemCopyBuffer = _OldCopyBuffer;
+			
+			
+		}
+		
+		
+		
+    }
+    
+    
+
+    public string UserFriendlifyAndEvaluate( string inToEval )
+    {
+
+
+
+        inToEval = inToEval.ToLower();
+
+		CompilerParameters cp = new CompilerParameters();
+		cp.ReferencedAssemblies.Add( "System.dll" );
+		//if we want to do more, we have to add UnityEngine and then find our assemblies in the library
+		//kinda overkill for now...
+		cp.GenerateExecutable = false;
+		cp.GenerateInMemory = true;
+		
+		//do weh ave to wrap all the functions we want to work? that's kinda annoying
+		string code = "using System;" +
+			"namespace Evaluator" +
+			"{ " +
+			"	public class CreatedEvaluator" +
+			"	{ " +
+			"		public string GetResult()" +
+			"		{ " +
+			"			return (" + inToEval + ").ToString( \"f4\" ); " +
+			"		} " +
+			"		public double sin( double inD ) { return Math.Sin( inD ); }	" +
+			"		public double cos( double inD ) { return Math.Cos( inD ); }	" +
+			"		public double tan( double inD ) { return Math.Tan( inD ); }	" +
+			"		public double asin( double inD ) { return Math.Asin( inD ); }	" +
+			"		public double acos( double inD ) { return Math.Acos( inD ); }	" +
+			"		public double atan( double inD ) { return Math.Atan( inD ); }	" +
+			"		public double pow( double inA, double inB ) { return Math.Pow( inA, inB ); }	" +
+			"		public double exp( double inD ) { return Math.Exp( inD ); }	" +
+			"		public double ln( double inD ) { return Math.Log( inD ); }	" +
+			"		public double log( double inD ) { return Math.Log10( inD ); }	" +
+			"		public double pi { get{ return Math.PI; } }	" +
+			"	} " +
+			"}";
+		
+		CompilerResults cr = _CSharpProvider.CompileAssemblyFromSource(cp, code );
+		
+		if (cr.Errors.HasErrors)
+		{
+			StringBuilder errors = new StringBuilder();
+			errors.Append("Error Calculating: ");
+			foreach( CompilerError ce in cr.Errors )
+			{
+				errors.AppendLine( ce.ErrorText );
+			}
+			Debug.LogError( errors.ToString() );
+			return null;
+		}
+		else
+		{		
+			Assembly a = cr.CompiledAssembly;
+			object createdEvaluator = a.CreateInstance( "Evaluator.CreatedEvaluator" );
+			MethodInfo mi = createdEvaluator.GetType().GetMethod( "GetResult" );
+  			return mi.Invoke( createdEvaluator, null ).ToString();
+		}
+    }
+  
+	
+	private static void StaticInit()
+	{
+		if( !_IsInitted )
+		{
+			_IsInitted = true;
+			
+			//on the mac we have to add mono into the path so it can be found
+			if( Application.platform == RuntimePlatform.OSXEditor )
+			{
+				string path = Environment.GetEnvironmentVariable( "PATH" );
+				path = path + ":" + EditorApplication.applicationPath + "/Contents/Frameworks/Mono/bin";
+				Environment.SetEnvironmentVariable( "PATH", path ); 
+				path = Environment.GetEnvironmentVariable( "PATH" );
+			}
+			
+			_CSharpProvider = new CSharpCodeProvider();
+		}
+	}
+	
+	string  		_ToEval;
+	EditorWindow 	_OldFocusedWindow;
+	int				_OldKeyboardControlID;
+	string			_OldCopyBuffer;
+	
+	private static bool _IsInitted = false;
+	private static CSharpCodeProvider _CSharpProvider;
+	
+	
+	
+}
